7eb02d52b3bb54f6e4a901235839a94b5784b797
czy <chenzy@zhejianglab.org>
2024-11-28 09:45:27 +0800

    version v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aster_core',
-    version='0.2',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
         'matplotlib==3.9.2',
